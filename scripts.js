0da8c764edb0157c1dc0b749a47f7bdcbf458b33
Jake Jepson <jwjepson1@gmail.com>
2022-08-27 03:29:28 -0400

    add an event listener to operator buttons

--- scripts.js
+++ scripts.js
@@ -15,21 +15,42 @@ function divide(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
     return operator(num1, num2);
 }
 
 let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
 let display = document.querySelector(".screen-data");
-let input = "";
+let num1;
+let operator = "";
 
 numbers.forEach((number) => {
     number.addEventListener("click", () => {
         if (((display.textContent).length + 1) > 12) {
             display.textContent += "";
         }
+        else if (num1) {
+            display.textContent = "";
+            display.textContent += number.textContent;
+
+        }
         else {
             display.textContent += number.textContent;
-            input += number.textContent;
         }
     });
 });
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        if (num1) {
+            
+            console.log(operator.value);
+            display.textContent = operate(operator.value, num1, display.textContent);
+        }
+        num1 = display.textContent;
+        console.log(num1);
+    });
+});
+
